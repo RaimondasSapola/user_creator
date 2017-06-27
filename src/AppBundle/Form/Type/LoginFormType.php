61d6fc9c001e16dffb48d41305e81e0918558282
Raimondas <raimondas.sapola@gmail.com>
2017-06-27 15:46:55 +0300

    second

--- src/AppBundle/Form/Type/LoginFormType.php
+++ src/AppBundle/Form/Type/LoginFormType.php
@@ -6,9 +6,10 @@ use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\Extension\Core\Type\SubmitType;
 use Symfony\Component\Form\Extension\Core\Type\TextType;
 use Symfony\Component\Form\Extension\Core\Type\PasswordType;
+use Symfony\Component\Form\FormBuilderInterface;
 
 class LoginFormType extends AbstractType{
-    public function buildForm(\Symfony\Component\Form\FormBuilderInterface $builder, array $options){
+    public function buildForm(FormBuilderInterface $builder, array $options){
         $builder
             ->add('email', TextType::class, [
                 'label' => false,
